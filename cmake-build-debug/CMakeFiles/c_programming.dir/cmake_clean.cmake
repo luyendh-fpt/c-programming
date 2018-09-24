a4af671814c83cd970b1ac85978748fc5af82164
LuyenDH <luyendh@fpt.edu.vn>
2018-09-24 18:44:38 +0700

    hello c

---
+++ cmake-build-debug/CMakeFiles/c_programming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_programming.dir/main.c.o"
+  "c_programming.pdb"
+  "c_programming"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
