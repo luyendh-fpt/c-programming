63375079549ef340d6df1c4d04b4ade5101b6c92
LuyenDH <luyendh@fpt.edu.vn>
2018-09-24 19:13:30 +0700

    Bai tap ngay 24/09

--- main.c
+++ main.c
@@ -3,5 +3,8 @@
 int main() {
     printf("Hello, World!\n");
     printf("Hello, new slide.\n");
+    printf("Hello, new slide.\n");
+    printf("Hello, new slide.\n");
+    printf("Hello, new slide.\n");
     return 0;
 }
