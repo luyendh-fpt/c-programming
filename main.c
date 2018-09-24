d432c0fb8dd765a8c4a49f3c14f1e56df006ada9
LuyenDH <luyendh@fpt.edu.vn>
2018-09-24 21:05:06 +0700

    Demo flowchart with code.

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+
 int main() {
-    printf("Câu lệnh này in ra màn hình đoạn text nằm trong nháy kép.\n");
-    printf("Tất cả các project c đều bắt đầu chạy từ hàm có tên là main.\n");
+    int num; // khai báo một "biến" kiểu số nguyên
+    printf("Please enter a number:"); // In ra màn hình đoạn text yêu cầu nhập số.
+    // Yêu cầu người dùng nhập từ bàn phím và lấy giá trị đó đưa vào
+    // biến num.
+    scanf("%d", &num);
+    // Kiểm tra nếu như num chia lấy dư cho 2 mà bằng 0
+    // thì in ra text "Number is even".
+    if (num % 2 == 0) {
+        printf("Number is even.");
+    }
+    // kết thúc chương trình.
     return 0;
 }
