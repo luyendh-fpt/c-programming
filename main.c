7ce127ac43a009b85fa668eb85fd20b5b633ad7b
LuyenDH <luyendh@fpt.edu.vn>
2018-09-24 19:16:28 +0700

    Cach dung ham printf

--- main.c
+++ main.c
@@ -2,5 +2,6 @@
 
 int main() {
     printf("Câu lệnh này in ra màn hình đoạn text nằm trong nháy kép.\n");
+    printf("Tất cả các project c đều bắt đầu chạy từ hàm có tên là main.\n");
     return 0;
 }
