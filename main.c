c4c3a213e0963565f272120fb7edb6bd2f26b440
LuyenDH <luyendh@fpt.edu.vn>
2018-10-01 18:22:16 +0700

    Switch case and loop.

--- main.c
+++ main.c
@@ -1,17 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int congHaiSo(int soThuNhat, int soThuHai) {
+    return soThuNhat + soThuHai;
+}
+
+int truHaiSo(int soThuNhat, int soThuHai) {
+    return soThuNhat - soThuHai;
+}
+
+int nhanHaiSo(int soThuNhat, int soThuHai) {
+    return soThuNhat * soThuHai;
+}
+
+float chiaHaiSo(int soThuNhat, int soThuHai) {
+    if (soThuHai == 0) {
+        printf("Khong the chia cho 0");
+        exit(1);
+    }
+    return (float) soThuNhat / soThuHai;
+}
+
+void taoMenu() {
+    printf("-------May tinh cua toi------:\n");
+    printf("1. Phep cong.\n");
+    printf("2. Phep tru.\n");
+    printf("3. Phep nhan.\n");
+    printf("4. Phep chia.\n");
+    printf("5. Thoat chuong trinh.\n");
+    printf("Vui long nhap lua chon cua ban (1|2|3|4|5):\n");
+}
 
 int main() {
-    int num; // khai báo một "biến" kiểu số nguyên
-    printf("Please enter a number:"); // In ra màn hình đoạn text yêu cầu nhập số.
-    // Yêu cầu người dùng nhập từ bàn phím và lấy giá trị đó đưa vào
-    // biến num.
-    scanf("%d", &num);
-    // Kiểm tra nếu như num chia lấy dư cho 2 mà bằng 0
-    // thì in ra text "Number is even".
-    if (num % 2 == 0) {
-        printf("Number is even.");
+    int soThuNhat, soThuHai, luaChon;
+    while (1 == 1) {
+        taoMenu();
+        scanf("%d", &luaChon);
+        printf("Vui long nhap so thu nhat: ");
+        scanf("%d", &soThuNhat);
+        printf("Vui long nhap so thu hai: ");
+        scanf("%d", &soThuHai);
+        switch (luaChon) {
+            case 1:
+                printf("Tong hai so %d va %d la: %d", soThuNhat, soThuHai, congHaiSo(soThuNhat, soThuHai));
+                break;
+            case 2:
+                printf("Hieu hai so %d va %d la: %d", soThuNhat, soThuHai, truHaiSo(soThuNhat, soThuHai));
+                break;
+            case 3:
+                printf("Tich hai so %d va %d la: %d", soThuNhat, soThuHai, nhanHaiSo(soThuNhat, soThuHai));
+                break;
+            case 4:
+                printf("Thuong hai so %d va %d la: %.2f", soThuNhat, soThuHai, chiaHaiSo(soThuNhat, soThuHai));
+                break;
+            case 5:
+                printf("Hen gap lai.");
+                exit(1);
+            default:
+                printf("Lua chon sai");
+                break;
+        }
+        printf("\n");
     }
-    // kết thúc chương trình.
     return 0;
 }
