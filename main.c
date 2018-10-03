218b299a1b12b04b48a4d7b219ce75ff91be1530
LuyenDH <luyendh@fpt.edu.vn>
2018-10-03 21:44:57 +0700

    con tro mang.

--- main.c
+++ main.c
@@ -2,14 +2,19 @@
 #include <stdlib.h>
 
 int main() {
-    int a = 10;
-    // khai báo biến con trỏ tên là pointe
-    // và có thể trỏ đến các biến số nguyên.
-    int *pointer;
-    pointer = &a; // gán địa chỉ của biến a cho con trỏ.
-    printf("Giá trị của a là %d\n", a); // In ra giá trị của a.
-    printf("Giá trị của con trỏ là %d\n", *pointer); // In 'giá trị con trỏ'
-    printf("Địa chỉ của con trỏ là %d\n", pointer); // In ra địa chỉ con trỏ.
-    printf("Địa chỉ của a là %d\n", &a); // In ra địa chỉ biến a.
+    int size = 5;
+    int *pointer = malloc(5 * sizeof(int));
+
+    for (int i = 0; i < size; ++i) {
+        printf("Nhap phan tu thu %d", i+1);
+        scanf("%d", pointer);
+        pointer++;
+    }
+
+    pointer--;
+    for (int j = 0; j < size; ++j) {
+        printf("%d ", *pointer);
+        pointer--;
+    }
     return 0;
 }
