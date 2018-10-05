370f51183bce744d4ef12947ac9c6166dac6d174
LuyenDH <luyendh@fpt.edu.vn>
2018-10-05 19:49:01 +0700

    working with array advanced.

--- main.c
+++ main.c
@@ -2,19 +2,45 @@
 #include <stdlib.h>
 
 int main() {
-    int size = 5;
-    int *pointer = malloc(5 * sizeof(int));
-
-    for (int i = 0; i < size; ++i) {
-        printf("Nhap phan tu thu %d", i+1);
+    int arrayNumber[5];
+    int sizeOfArray = sizeof(arrayNumber) / sizeof(int);
+    // Cách 1. Làm việc thông qua index của mảng.
+    for (int i = 0; i < sizeOfArray; ++i) {
+        printf("Vui lòng nhập giá trị cho phần tử thứ %d", i + 1);
+        scanf("%d", &arrayNumber[i]);
+    }
+    for (int j = 0; j < sizeOfArray; ++j) {
+        printf("%d\n", arrayNumber[j]);
+    }
+    // Cách 2. Làm việc thông qua con trỏ và di chuyển con trỏ. p++, p--.
+    int *pointer = arrayNumber;
+    for (int i = 0; i < sizeOfArray; ++i) {
         scanf("%d", pointer);
         pointer++;
     }
-
     pointer--;
-    for (int j = 0; j < size; ++j) {
+    for (int j = 0; j < sizeOfArray; ++j) {
         printf("%d ", *pointer);
         pointer--;
+    }
+
+    // Cách 3. Làm việc thông qua con trỏ nhưng không di chuyển mà thông
+    // qua toán tử p + i;
+    int *pointer2 = &arrayNumber[0];
+    for (int i = 0; i < sizeOfArray; ++i) {
+        scanf("%d", pointer2 + i);
+    }
+
+    for (int j = 0; j < sizeOfArray; ++j) {
+        printf("%d\n", *(pointer2 + j));
+    }
+
+    // Cách 4. Làm việc với mảng thông qua con trỏ (chính là tên của mảng đó.)
+    for (int i = 0; i < sizeOfArray; ++i) {
+        scanf("%d", arrayNumber + i);
+    }
+    for (int j = 0; j < sizeOfArray; ++j) {
+        printf("%d\n", *(arrayNumber + j));
     }
     return 0;
 }
