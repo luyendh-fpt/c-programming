52fbf9781e5eda5540552374055d8de034371629
LuyenDH <luyendh@fpt.edu.vn>
2018-10-05 20:32:13 +0700

    Malloc demo

--- main.c
+++ main.c
@@ -2,9 +2,21 @@
 #include <stdlib.h>
 
 int main() {
-    int i = 10;
-//    int j = ++i;
-    int j = i++;
-    printf("%d", j);
+    int sizeOfArray;
+    printf("Vui lòng nhập số lượng phần tử");
+    scanf("%d", &sizeOfArray);
+
+    int *pointer = (int *) malloc(sizeOfArray * sizeof(int));
+    if (pointer == NULL) {
+        printf("Không đủ bộ nhớ");
+        return 0;
+    }
+    for (int i = 0; i < sizeOfArray; ++i) {
+        printf("Nhập gía trị cho phần tử thứ %d", i + 1);
+        scanf("%d", pointer + i);
+    }
+    for (int j = 0; j < sizeOfArray; ++j) {
+        printf("Gía trị tại phần tử thứ %d là %d\n", j + 1, *(pointer + j));
+    }
     return 0;
 }
