898bd1f9c69e2713fdb3a17b0a1b243a5ef29d83
LuyenDH <luyendh@fpt.edu.vn>
2018-10-15 21:26:20 +0700

    Struct demo

--- main.c
+++ main.c
@@ -2,72 +2,42 @@
 #include <stdlib.h>
 #include <memory.h>
 
+struct SinhVien {
+    char ten[50];
+    char email[50];
+    char soDienThoai[20];
+    int tuoi;
+};
+
+void removeStdChar(char array[]) {
+    if (strchr(array, '\n') == NULL) {
+        while (fgetc(stdin) != '\n');
+    }
+}
+
 int main() {
-    // 1. Yêu cầu nhập tên.
-    // 2. In ra độ dài của tên.
-    // 3. Yêu cầu nhập họ.
-    // 4. In ra độ dài của họ.
-    // 5. Nối tên vào họ. In ra kết qủa sau khi nối.
-
-    // Khai báo mảng 20 ký tự chứa họ người dùng.
-    char ho[20];
-    printf("Vui lòng nhập họ: \n");
-    // fgets(nhập vào đâu, nhập bao nhiêu, lấy từ đâu);
-    fgets(ho, sizeof(ho) * sizeof(char), stdin);
-    // fflush(sdtin);
-    // Tìm trong họ ký tự xuống dòng strchr(ho, '\n').
-//    if (!strchr(ho, '\n')) {
-//        do {
-//            char charInStdin = fgetc(stdin);
-//            printf("'%c'\n", charInStdin);
-//            if(charInStdin != '\n'){
-//                printf("Không phải \\n\n");
-//            }else{
-//                printf("Tìm thấy \\n\n");
-//                break;
-//            }
-//        }while(1);
-//    }
-    if (!strchr(ho, '\n')) {
-        while (fgetc(stdin) != '\n');
+    struct SinhVien sinhViens[2];
+    for (int i = 0; i < 2; ++i) {
+        printf("Vui lòng nhập tên sinh viên thứ %d", i + 1);
+        fgets(sinhViens[i].ten, sizeof(sinhViens[i].ten) * sizeof(char), stdin);
+        removeStdChar(sinhViens[i].ten);
+        printf("Vui lòng nhập email sinh viên thứ %d", i + 1);
+        fgets(sinhViens[i].email, sizeof(sinhViens[i].email) * sizeof(char), stdin);
+        removeStdChar(sinhViens[i].email);
+        printf("Vui lòng nhập phone sinh viên thứ %d", i + 1);
+        fgets(sinhViens[i].soDienThoai, sizeof(sinhViens[i].soDienThoai) * sizeof(char), stdin);
+        removeStdChar(sinhViens[i].soDienThoai);
+        printf("Vui lòng nhập tuổi sinh viên thứ %d", i + 1);
+        scanf("%d", &sinhViens[i].tuoi);
+        getchar();
     }
 
-    // in ra độ dài của họ => strlen
-    // Tính ra độ dài của họ vừa nhập.
-    int doDaiCuaHo = strlen(ho);
-    printf("Độ dài của họ là %d\n", doDaiCuaHo);
-
-    // Remove dấu xuống dòng nằm trong họ.
-    ho[strlen(ho) - 1] = ' ';
-
-    char ten[20];
-    printf("Vui lòng nhập tên: \n");
-    fgets(ten, sizeof(ten) * sizeof(char), stdin);
-//    if (!strchr(ho, '\n')) {
-//        do {
-//            char charInStdin = fgetc(stdin);
-//            printf("'%c'\n", charInStdin);
-//            if(charInStdin != '\n'){
-//                printf("Không phải \\n\n");
-//            }else{
-//                printf("Tìm thấy \\n\n");
-//                break;
-//            }
-//        }while(1);
-//    }
-    if (!strchr(ho, '\n')) {
-        while (fgetc(stdin) != '\n');
+    for (int i = 0; i < 2; ++i) {
+        printf("Tên sinh viên %s\n", sinhViens[i].ten);
+        printf("Số điện thoại %s\n", sinhViens[i].soDienThoai);
+        printf("Email %s\n", sinhViens[i].email);
+        printf("Tuổi %d\n", sinhViens[i].tuoi);
     }
-    int doDaiCuaTen = strlen(ten);
-    printf("Độ dài của họ là %d\n", doDaiCuaTen);
 
-    // Tạo ra một mảng ký tự mới có size có thể chứa được cả họ và tên.
-    char hovaten[doDaiCuaHo + doDaiCuaTen];
-    // Copy thông tin từ mảng họ vào mảng mới
-    strcpy(hovaten, ho);
-    // Nối tên vào mảng mới
-    strcat(hovaten, ten);
-    // In ra.
-    printf("Họ và tên vừa nhập: %s", hovaten);
     return 0;
 }
